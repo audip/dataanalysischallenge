71fd55f7e742365b046059fe28b211e374e001e7
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-22 23:35:29 -0600

    Adds state birth counts correctly

--- problem1.py
+++ problem1.py
@@ -3,15 +3,12 @@
 import os
 import zipfile
 import operator
-import requests
 
 dictionary_namecount = {}
 progress = 0
 total_names = 5647426
-archive_link = "http://www.ssa.gov/oact/babynames/state/namesbystate.zip"
-file_record = requests.get(archive_link)
 
-with zipfile.ZipFile(file_record) as namesbystate:
+with zipfile.ZipFile('namesbystate.zip') as namesbystate:
     for filename in namesbystate.namelist():
         if not os.path.isdir(filename) and filename != 'StateReadMe.pdf':
             # if m.endswith('.mp3'):
@@ -21,21 +18,12 @@ with zipfile.ZipFile(file_record) as namesbystate:
                     # processing line by line
                     str = line.split(',')
                     baby_name = str[3]
+                    birth_count = int(str[4].splitlines()[0])
                     progress = progress + 1
                     if baby_name not in dictionary_namecount:
-                        dictionary_namecount[baby_name] = 1
+                        dictionary_namecount[baby_name] = birth_count
                     else:
-                        dictionary_namecount[baby_name] += 1
-
-# def keywithmaxval(d):
-#     # Fastest key value sorter approach
-#     # Key: Baby's name, Value: Count of name occurence
-#     name_count=list(d.values())
-#     dict_key=list(d.keys())
-#     return dict_key[name_count.index(max(name_count))]
-#
-# highest_key = keywithmaxval(dictionary_namecount)
-# print highest_key
+                        dictionary_namecount[baby_name] += birth_count
 
 sorted_list = sorted(dictionary_namecount.items(), key=operator.itemgetter(1))
 sorted_list.reverse()
