45415140257e5509a7d93effd2e4a2f472cd77ef
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-24 01:00:36 -0600

    Basic structure and line read from industrial data

--- problem3.py
+++ problem3.py
@@ -4,44 +4,61 @@ import os
 import operator
 
 fixtureName = 'fixtures/Sample_DSC_Data.txt'
+path = 'fixtures/'
 
 class industrialDetails(object):
     def __init__(self, rawData):
             parts = rawData.split(' ')
             # Parses values into class variables
-            self.state = parts[0].strip()
-            self.gender = parts[1].strip()
-            self.year = parts[2].strip()
-            self.name = parts[3].strip()
-            self.birthCount = int(parts[4].strip())
+            self.time = int(parts[0].strip())
+            self.temperature = int(parts[1].strip())
+            self.heatFlow = int(parts[2].strip())
+            self.heatCapacity = int(parts[3].strip())
+            self.samplePurgeFlow = int(parts[4].strip())
+            self.pressure = int(parts[5].strip())
 
     def printsOutput(self):
-        print self.state+","+self.gender+","+self.year+","+self.name+","+str(self.birthCount)
+        print str(self.time)+","+str(self.temperature)+","+str(self.heatFlow)+","+str(self.heatCapacity)+","+str(self.samplePurgeFlow)+","+str(self.pressure)
     def getAll(self):
-        return self.state+","+self.gender+","+self.year+","+self.name+","+str(self.birthCount)
-    def getName(self):
-        return self.name
-    def getState(self):
-        return self.state
-    def getGender(self):
-        return self.gender
-    def getYear(self):
-        return self.year
-    def getBirthCount(self):
-        return self.birthCount
+        return str(self.time)+","+str(self.temperature)+","+str(self.heatFlow)+","+str(self.heatCapacity)+","+str(self.samplePurgeFlow)+","+str(self.pressure)
+    def getTime(self):
+        return self.time
+    def getTemperature(self):
+        return self.temperature
+    def getHeatFlow(self):
+        return self.heatFlow
+    def getheatCapacity(self):
+        return self.heatCapacity
+    def getSamplePurgeFlow(self):
+        return self.samplePurgeFlow
+    def getPressure(self):
+        return self.pressure
 
-def dataUnload(file_name, yearOfBirth):
-    bornInYear = []
+def dataUnload(file_name):
     # Unzips the file and return babies born in 2013
-    with zipfile.ZipFile(file_name) as namesbystate:
-        for filename in namesbystate.namelist():
-            extension = os.path.splitext(filename)[1][1:].strip().lower()
-            if not os.path.isdir(filename) and extension == 'txt':
-                # read the file & validates for txt files only
-                with namesbystate.open(filename) as f:
-                    for line in f:
-                        # processing line by line
-                        baby = babyDetails(line)
-                        if baby.getYear() in yearOfBirth:
-                            bornInYear.append(baby.getAll())
-    return bornInYear
+    counter = 0
+    lineToRead = 0
+    for filename in os.listdir(path):
+        extension = os.path.splitext(filename)[1][1:].strip().lower()
+        if not os.path.isdir(filename) and extension == 'txt':
+            filename = os.path.join(path, filename)
+            with open(filename, "r") as f:
+                for line in f:
+                    # processing line by line
+                    # print "hello"
+                    if line.strip() == "StartOfData":
+                        lineToRead = 1
+                        # Skips the StartOfData line
+                        continue
+                    if lineToRead == 1:
+                        counter += 1
+                        print line
+                    if counter > 100:
+                        break
+
+def main():
+    print "1. Main program initiated"
+    dataUnload(fixtureName)
+    print "n. Main program finished"
+
+if __name__ == "__main__":main()
