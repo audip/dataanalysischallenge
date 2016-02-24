8aafec2cdc31221d279c134f1c777eb372c38260
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-24 00:31:09 -0600

    Problem1 cleaned

--- problem1.py
+++ problem1.py
@@ -1,31 +1,71 @@
 #!/usr/bin/python
+# Python 2.7
 
 import os
 import zipfile
 import operator
 
-dictionary_namecount = {}
-progress = 0
-total_names = 5647426
+fixtureName = 'fixtures/namesbystate.zip'
 
-with zipfile.ZipFile('namesbystate.zip') as namesbystate:
-    for filename in namesbystate.namelist():
-        if not os.path.isdir(filename) and filename != 'StateReadMe.pdf':
-            # if m.endswith('.mp3'):
-            # read the file
-            with namesbystate.open(filename) as f:
-                for line in f:
-                    # processing line by line
-                    str = line.split(',')
-                    baby_name = str[3]
-                    birth_count = int(str[4].splitlines()[0])
-                    progress = progress + 1
-                    if baby_name not in dictionary_namecount:
-                        dictionary_namecount[baby_name] = birth_count
-                    else:
-                        dictionary_namecount[baby_name] += birth_count
+class babyDetails(object):
+    def __init__(self, rawData):
+            parts = rawData.split(',')
+            # Parses values into class variables
+            self.state = parts[0].strip()
+            self.gender = parts[1].strip()
+            self.year = parts[2].strip()
+            self.name = parts[3].strip()
+            self.birthCount = int(parts[4].strip())
 
-sorted_list = sorted(dictionary_namecount.items(), key=operator.itemgetter(1))
-sorted_list.reverse()
-for loop_var in range(5):
-    print sorted_list[loop_var]
+    def printsOutput(self):
+        print self.state+","+self.gender+","+self.year+","+self.name+","+str(self.birthCount)
+    def getAll(self):
+        return self.state+","+self.gender+","+self.year+","+self.name+","+str(self.birthCount)
+    def getName(self):
+        return self.name
+    def getState(self):
+        return self.state
+    def getGender(self):
+        return self.gender
+    def getYear(self):
+        return self.year
+    def getBirthCount(self):
+        return self.birthCount
+
+def dataUnload(file_name):
+    """ Unwraps data from the dataset using filename """
+    dictionary_namecount = {}
+    print "2. File read initiated"
+    # Unzips the file and return babies born in 2013
+    with zipfile.ZipFile(file_name) as namesbystate:
+        for filename in namesbystate.namelist():
+            extension = os.path.splitext(filename)[1][1:].strip().lower()
+            if not os.path.isdir(filename) and extension == 'txt':
+                # read the file & validates for txt files only
+                with namesbystate.open(filename) as f:
+                    for line in f:
+                        # processing line by line
+                        baby = babyDetails(line)
+                        if baby.getName() not in dictionary_namecount:
+                            dictionary_namecount[baby.getName()] = baby.getBirthCount()
+                        else:
+                            dictionary_namecount[baby.getName()] += baby.getBirthCount()
+    print "3. File read completed"
+    return dictionary_namecount
+
+def top5(dict_namecount = {}):
+    """ Takes dictionary as input and prints top five name """
+    print "4. Sorting the list to find the top 5"
+    sorted_list = sorted(dict_namecount.items(), key=operator.itemgetter(1))
+    sorted_list.reverse()
+    for loop_var in range(5):
+        print sorted_list[loop_var]
+
+def main():
+    print "5 steps in total"
+    print "1. Main program initiated"
+    dictionary_data = dataUnload(fixtureName)
+    top5(dictionary_data)
+    print "5. Main program finished"
+
+if __name__ == "__main__":main()
