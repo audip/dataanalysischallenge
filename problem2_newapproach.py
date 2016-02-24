9275e375d7d0ee3c5f946c0fb875368baf69fd26
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-23 21:51:57 -0600

    Modular functions built

--- problem2_newapproach.py
+++ problem2_newapproach.py
@@ -7,10 +7,9 @@ import operator
 dictionary_namecount = {}
 
 class babyDetails(object):
-
     def __init__(self, rawData):
             parts = rawData.split(',')
-
+            # Parses values into class variables
             self.state = parts[0].strip()
             self.gender = parts[1].strip()
             self.year = parts[2].strip()
@@ -19,6 +18,17 @@ class babyDetails(object):
 
     def printsOutput(self):
         print self.state+"::"+self.gender+"::"+self.year+"::"+self.name+"::"+str(self.birthCount)
+
+    def getName(self):
+        return self.name
+    def getState(self):
+        return self.state
+    def getGender(self):
+        return self.gender
+    def getYear(self):
+        return self.year
+    def getBirthCount(self):
+        return self.birthCount
 
 with zipfile.ZipFile('fixtures/namesbystate.zip') as namesbystate:
     for filename in namesbystate.namelist():
@@ -29,7 +39,7 @@ with zipfile.ZipFile('fixtures/namesbystate.zip') as namesbystate:
                 for line in f:
                     # processing line by line
                     baby = babyDetails(line)
-                    baby.printsOutput()
+                    print baby.getBirthCount()
 
 sorted_list = sorted(dictionary_namecount.items(), key=operator.itemgetter(1))
 sorted_list.reverse()
