b2e15a04f35353a0ca100552e1373d4f5dade1e3
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-25 21:16:39 -0600

    Flask basic code

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def dashboard():
+    """Respond with dashboard web page"""
+    return redirect("./dashboard/index.html", code=302)
+
+@app.route('/problem1')
+def problem1():
+    return 'problem1 Page'
+
+@app.route('/problem2')
+def problem2():
+    return 'problem2 Page'
+
+@app.route('/problem3')
+def problem3():
+    return 'problem3 Page'
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
